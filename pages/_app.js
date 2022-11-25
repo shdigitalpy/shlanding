0c5092bd5dcacf61a2fbbd61560fb86e0d491edc
Sandro Huber <sandro@sh-digital.ch>
2022-11-25 16:56:04 +0100

    noindex

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
         <link rel="canonical" href={canonicalURL} />
 
+        <meta name="robots" content="noindex" />
             
 
   
